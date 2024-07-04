6f03fb92b3abf599f25ac209fb29fa272af1f6ef
JakobSSS <jakobschr2@gmail.coma>
2024-07-04 10:48:30 +0200

    urls angepasst

--- app/core/urls.py
+++ app/core/urls.py
@@ -7,11 +7,9 @@ urlpatterns = [
     path('userid', UserIDListAPIView.as_view()),
     path('useriddetail/<int:id>', UserIDListDetailsAPIView.as_view()),
     path('answers', AnswersAPIView.as_view()),
-    path('login', LoginUserAPIView.as_view()),
+    path('login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('register', RegisterUserAPIView.as_view(), name='register'),
     path('user', UserView.as_view()),
     path('logout', LogoutUserAPIView.as_view(), name='logout'),
-    path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
-
 ]
